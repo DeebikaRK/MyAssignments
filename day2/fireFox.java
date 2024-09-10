fb7256247ed65e8fe4017a1a121b4a12cf7ceb5d
DeebikaRK <deebikark@gmail.com>
2024-09-10 19:51:18 +0530

    Add files via upload
    week1/day2

---
+++ day2/fireFox.java
@@ -0,0 +1,16 @@
+package week1.day2;
+
+public class fireFox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 float browserVersion = 100.2f;
+		 String browserName = "firefox";
+		 boolean isVisible = true;
+		 int releaseYear = 1998;
+		 char browserLogo = 'f';
+		 System.out.println("Browser Version = "+browserVersion+"\nBrowser Name = "+browserName+"\nIs Visible = "+isVisible+"\nRelease Year = "+releaseYear+"\nBrowser Logo = "+browserLogo);
+		
+	}
+
+}
